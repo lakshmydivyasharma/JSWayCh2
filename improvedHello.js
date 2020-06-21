2fc96310d61f382efe5b1cb55e3a4013d2abc7da
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-06-21 17:38:09 -0400

    updated v

---
+++ improvedHello.js
@@ -0,0 +1,7 @@
+Improved hello
+
+// Write a program that asks the user for their first name and last name.
+//The program then displays them in one sentence.
+
+const name = prompt("Enter your first and last name:");
+alert(`My name is ${name}`)
