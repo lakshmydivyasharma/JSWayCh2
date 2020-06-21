d411c5bd237b82a909e117318721c499edfa2602
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-06-21 17:47:22 -0400

    updated v

---
+++ VATcalculation.js
@@ -0,0 +1,7 @@
+
+// VAT calculation
+// Write a program that asks the user for a raw price.
+//After that, it calculates the corresponding final price using a VAT rate of 20.6%.
+
+let price = prompt("How much does your item cost?")
+alert(`After tax, your item now costs ${price * 1.26}`)
